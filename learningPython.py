abea4be168f630e5ed3548fa7e9eb5c60b0fa6b5
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-03-07 01:22:58 +0600

    working again

---
+++ learningPython.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_axis = np.array([1,2,6,8])
+y_axis = np.array([3,7,8,10])
+
+# plt.plot(x_axis,y_axis)
+# plt.show()
+
+plt.plot(y_axis)
+plt.show()
---
+++ qtFive.py
@@ -0,0 +1,2 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
